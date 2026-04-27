f1c23c9f1a9ed96c8231328db179daeb13bef38a
dhaifx <dhaifzlabang@gmail.com>
2026-04-27 21:06:25 +0800

    update pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -60,19 +60,19 @@ function App() {
             </ProtectedRoute>
           } />
           <Route path="/staff" element={
-            // <ProtectedRoute allowedRoles={['Staff', 'SuperAdmin']}>
+            <ProtectedRoute allowedRoles={['Staff', 'SuperAdmin']}>
               <StaffDashboard />
-            // </ProtectedRoute>
+            </ProtectedRoute>
           } />
           <Route path="/director" element={
-            // <ProtectedRoute allowedRoles={['Director', 'SuperAdmin']}>
+            <ProtectedRoute allowedRoles={['Director', 'SuperAdmin']}>
               <DirectorDashboard />
-            // </ProtectedRoute>
+            </ProtectedRoute>
           } />
           <Route path="/superadmin" element={
-            // <ProtectedRoute allowedRoles={['SuperAdmin']}>
+            <ProtectedRoute allowedRoles={['SuperAdmin']}>
               <SuperAdminDashboard />
-            // </ProtectedRoute>
+            </ProtectedRoute>
           } />
 
           {/* Legacy /student route redirects to login */}
